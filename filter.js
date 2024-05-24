2a48c178b319c255273a516fecab5f01f07500e2
smark <ynlbq@outlook.com>
2024-05-24 09:16:21 +0800

    chore: Update Redis dependency to version 4.6.14

--- filter.js
+++ filter.js
@@ -1,43 +1,45 @@
-import { createClient } from 'redis';
-
-const redisServer = process.env.KV_URL || "redis://127.0.0.1:6379";
-
-const client = createClient({
-  url: redisServer
-});
-
-client.connect();
+import { kv } from '@vercel/kv';
 
 // 初始化余额并设置过期时间为31天
-export function initBalance(key, balance) {
-  console.log("initBalance", key, balance)
-  console.log(redisServer)
-  return client.setEx(key, 2678400, balance);
+export async function initBalance(key, balance) {
+  console.log("initBalance", key, balance);
+  await kv.set(key, balance, { ex: 2678400 });
 }
 
 // 获取余额
-export function getBalance(key) {
-  return client.get(key).then(balance => balance).catch(() => false);
+export async function getBalance(key) {
+  try {
+    const balance = await kv.get(key);
+    return balance;
+  } catch (error) {
+    return false;
+  }
 }
 
 // 扣除余额
-export function deductBalance(key, amount) {
-  return client.decrBy(key, amount).then(balance => balance).catch(() => false);
+export async function deductBalance(key, amount) {
+  try {
+    const balance = await kv.get(key);
+    const newBalance = balance - amount;
+    await kv.set(key, newBalance);
+    return newBalance;
+  } catch (error) {
+    return false;
+  }
 }
 
 // 检查当前余额是否足够
-export function checkBalance(key, text) {
-  return new Promise((resolve, reject) => {
-    client.get(key).then(balance => {
-      if (balance >= countBytes(text)) {
-        resolve(true);
-      } else {
-        resolve(false);
-      }
-    }).catch(() => {
-      reject(false);
-    });
-  });
+export async function checkBalance(key, text) {
+  try {
+    const balance = await kv.get(key);
+    if (balance >= countBytes(text)) {
+      return true;
+    } else {
+      return false;
+    }
+  } catch (error) {
+    return false;
+  }
 }
 
 // 计算当前字节数
